c58a4e0e6c2a9264e8d4bea0edc1b9f80a05abd9
jonekell1002 <jonekell1002@gmail.com>
2022-02-17 13:26:12 +0100

    Initial project

---
+++ src/sector/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "../css/main.sector.scss";
+
+const MainSector = () => {
+  return (
+    <div className="div-main">
+
+    </div>
+  )
+}
+
+export default MainSector;
